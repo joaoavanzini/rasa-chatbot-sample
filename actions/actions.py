5eaa1a2521257337037f54e7dd45bcc7e6cccf4f
victorbalbinoaraujo <73204740+victorbalbinoaraujo@users.noreply.github.com>
2021-06-22 13:04:05 -0300

    Add files via upload

---
+++ actions/actions.py
@@ -0,0 +1,44 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker, FormValidationAction
+
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+
+class ValidateNameForm(FormValidationAction):
+
+    def name(self) -> Text:
+        return "validate_name_form"
+    
+    def validate_first_name(self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `first_name` value."""
+        return {"first_name": slot_value}
+    
+        
