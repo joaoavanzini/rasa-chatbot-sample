a8a1388786f79c45e114c423ce4fab377c9965ee
caio-gferreira <caiogfferreira@gmail.com>
2021-06-22 14:22:03 -0300

    criando slots e formulário

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,30 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+from rasa_sdk.forms import FormValidationAction
+from rasa_sdk.types import DomainDict
+from rasa_sdk.events import SlotSet 
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
 #
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "validate_nome"
+
+    def run(self, 
+            dispatcher: CollectingDispatcher,
+            slot_value: Any,
+            tracker: Tracker,
+            domain: DomainDict,
+            )-> Dict[Text, Any]:
+        
+        nome = slot_value
+
+        # Imprimir hello world para o usuário
+        dispatcher.utter_message(text=nome)
+
+        return []
