b9c1cba301db872995d2a9279d991d071b67d331
caio-gferreira <caiogfferreira@gmail.com>
2021-07-08 10:04:39 -0300

    Acrescentando as entidades

--- actions/action_fallback.py
+++ actions/action_fallback.py
@@ -29,50 +29,8 @@ class ActionDefaultFallback(Action):
             domain: Dict[Text, Any],
             ) -> List[Dict[Text, Any]]:
         
-        text = tracker.latest_message['text']
-        
-
-        variacao = choice(
-            [
-                "Ops...",
-                "Poxa...",
-                "Poxa vida...",
-                "Foi mal...",
-                "Caramba..."
-            ]
-        )
-
-        events = [e['name'] for e in tracker.events if e['event'] == 'action' and e['name'] not in ['action_listen']]
-        if events[-1] != self.name():
-            dispatcher.utter_message(
-                choice(
-                    [
-                        f"{variacao} me desculpe, mas não consegui entender.",
-                        "Desculpe, mas não consegui entender o que você disse.",
-                        "Não entendi muito bem o que você disse, ainda estou aprendendo.",
-                        f"{variacao}, não entendi o que você quis dizer, tente repetir de outra forma.",
-                        f"{variacao}, não achei o que você está procurando."
-                    ]
-                )
-            )
-            dispatcher.utter_message("Tente me escrever o que precisa com outras palavras...")
-        else:
-            if events[-2] != self.name():
-                dispatcher.utter_message(
-                    choice(
-                        [
-                            f"{variacao} ainda não consegui entender.",
-                            "Ainda não consegui entender.",
-                            "Ainda não ficou muito claro.",
-                            "Ainda não achei o que você está procurando.",
-                            f"{variacao} ainda não entendi."
-                        ]
-                    )
-                )
-                dispatcher.utter_message("Tente me escrever o que precisa com outras palavras...")
-            else:
-                dispatcher.utter_message("Realmente não consegui entender. 😕")
-                dispatcher.utter_message("Tentaremos a conversa novamente mais tarde.")
+        text = tracker.get_intent_of_latest_message()
+        print (text)
 
         # Revert user message which led to fallback.
-        return [UserUtteranceReverted()]
+        return []
